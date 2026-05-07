7261c9a51080a2eef03e277e7973c2ddc9b7481f
necruda <andradealejandro1106@gmail.com>
2026-05-06 22:56:59 -0500

    added name to simple calc

--- python/simple_calc/simple_calc.py
+++ python/simple_calc/simple_calc.py
@@ -4,7 +4,7 @@
 Simple Calculator
 --------------------------------------------------------------------------
 License:   
-Copyright 2026 - <NAME>
+Copyright 2026 - Alejandro Andrade
 
 Redistribution and use in source and binary forms, with or without 
 modification, are permitted provided that the following conditions are met:
